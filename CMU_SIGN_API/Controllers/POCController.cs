8d6110ae3fbf2ff7919315e8d5182ee214d64a62
CHEEWIN BUAPAN <cheewin.b@cmu.ac.th>
2022-06-08 12:29:44 +0700

    add  endpoint  [HttpPost("v1/sign")]

--- CMU_SIGN_API/Controllers/POCController.cs
+++ CMU_SIGN_API/Controllers/POCController.cs
@@ -1,4 +1,5 @@
 ﻿
+using CMU_SIGN_API.Model;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +11,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace CMU_SING_API.Controllers
@@ -21,21 +23,58 @@ namespace CMU_SING_API.Controllers
     {
         public POCController(ILogger<ITSCController> logger, IHttpClientFactory clientFactory, IWebHostEnvironment env)
         {
-
-            this.loadConfig(logger, clientFactory, env);    
+            this.loadConfig(logger, clientFactory, env);
         }
 
-        [HttpPost("v1/sing")]
-        public async Task<IActionResult> sing(IFormFile filename, [FromHeader] String pass_phase, [FromHeader] String ref_id, [FromHeader] String sigfield, [FromHeader] String reason)
+        [HttpPost("v1/sign")]
+        public async Task<IActionResult> sign(IFormFile filename, [FromHeader] String pass_phase, [FromHeader] String ref_id, [FromHeader] String sigfield, [FromHeader] String reason)
         {
-            String Cmuaccount = "";
-            Cmuaccount = await this.getCmuaccount();
-            if (Cmuaccount == "unauthorized") { return Unauthorized(); }
+            try
+            {
+                String Cmuaccount = "";
+                Cmuaccount = await this.getCmuaccount();
+                if (Cmuaccount == "unauthorized") { return Unauthorized(); }
+
+                String webhook = Environment.GetEnvironmentVariable("WEBHOOK");
+                MultipartFormDataContent multipartFormContent = new MultipartFormDataContent();
+                Stream stream = new MemoryStream();
+                filename.CopyTo(stream);
+                var fileStreamContent = new StreamContent(stream);
+                multipartFormContent.Add(fileStreamContent, name: "pdf", fileName: "test.pdf");
+
+                multipartFormContent.Add(new StringContent("accesstoken"), this._accesstoken);
+                multipartFormContent.Add(new StringContent("pass_phase"), pass_phase);
+                multipartFormContent.Add(new StringContent("ref_id"), ref_id);
+                multipartFormContent.Add(new StringContent("sigfield"), sigfield);
+                multipartFormContent.Add(new StringContent("reason"), reason);
+
+                multipartFormContent.Add(new StringContent("webhook"), webhook);
+
+                String SIGNAPI = Environment.GetEnvironmentVariable("SINGAPI");
+                HttpClient httpClient = _clientFactory.CreateClient();
+                HttpResponseMessage response = await httpClient.PostAsync(SIGNAPI, multipartFormContent);
+                var responseString = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    responseString = await response.Content.ReadAsStringAsync();
+                    SignModel signModel = JsonConvert.DeserializeObject<SignModel>(responseString);
+                    return Ok();
+                }
+                else
+                {
+
+                    String textReturn = "" + (int)response.StatusCode + " " + responseString;
+                    return Ok(textReturn);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.StackTrace);
+            }
 
 
 
 
-            return Ok();
         }
     }
 }
---
+++ CMU_SIGN_API/Model/SignModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMU_SIGN_API.Model
+{
+    public class SignModel
+    {
+        public string status { get; set; }
+        public string ref_id { get; set; }
+        public string filename { get; set; }
+    }
+}
