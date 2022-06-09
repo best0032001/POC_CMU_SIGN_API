dc159dcfe38f7dc8ac4aedafbf0bcb6bfc22e46f
CHEEWIN BUAPAN <cheewin.b@cmu.ac.th>
2022-06-09 19:44:29 +0700

    fix EFCore

--- CMU_SIGN_API/Model/ApplicationDBContext.cs
+++ CMU_SIGN_API/Model/ApplicationDBContext.cs
@@ -12,7 +12,5 @@ namespace CMU_SIGN_API.Model
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
-
-        public DbSet<SignRequest> SignRequests { get; set; }
     }
 }
